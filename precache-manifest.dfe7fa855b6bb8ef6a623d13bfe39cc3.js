809dec39afebcef6cf36c820a8bcfb634868e4a4
Fredrik Norén <fredrik.jw.noren@gmail.com>
2020-06-07 16:48:31 +0200

    Updates

---
+++ precache-manifest.dfe7fa855b6bb8ef6a623d13bfe39cc3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e19dd25df83a3c168fd2313c102157c0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9dc0ce5451a9c3cf517a",
+    "url": "/static/css/main.c6a6fa11.chunk.css"
+  },
+  {
+    "revision": "4433070cbbb2a1f0fa42",
+    "url": "/static/js/2.f85d0704.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.f85d0704.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc0ce5451a9c3cf517a",
+    "url": "/static/js/main.651be749.chunk.js"
+  },
+  {
+    "revision": "053b902a79988c74797a",
+    "url": "/static/js/runtime-main.4bc9b1e6.js"
+  }
+]);

74ceb3c4e50918f2329e4aa316bcc75a1dd72b7e
Fredrik Norén <fredrik.jw.noren@gmail.com>
2020-06-08 10:16:18 +0200

    Rename

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <div className="Title">
-        Monte Carlo Labs
+        Cloud Mountain Labs
       </div>
       <div className="Links">
         <a href="https://derkgame.com">Dr. Derk's Mutant Battlegrounds</a>

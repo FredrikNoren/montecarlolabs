9247606736c8fdd142e04a975172800dbc4f6a82
Fredrik Norén <fredrik.jw.noren@gmail.com>
2020-06-14 14:52:23 +0200

    Use svg

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import LogoPng from './MountRoukeLogo.png';
+import LogoSvg from './MountRoukeLogo.svg';
 
 function App() {
   return (
     <div className="App">
-      <img src={LogoPng} className="Logo" alt="Mount Rouke Studios" />
+      <img src={LogoSvg} className="Logo" alt="Mount Rouke Studios" />
       <div className="Links">
         <a href="https://derkgame.com">Dr. Derk's Mutant Battlegrounds</a>
       </div>
